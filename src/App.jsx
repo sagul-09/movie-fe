fb86c3c8908cc02bd4684eae51563e50a4cda3af
sagul-09 <nihal.evil.royal@gmail.com>
2023-06-26 10:54:35 +0530

    movie suggestion Front end

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import GetAllMoviesComponent from "./components/GetAllMoviesComponent/GetAllMoviesComponent";
+import AddNewMovieComponent from "./components/AddNewMovieComponent/AddNewMovieComponent";
+import MovieSuggestionComponent from "./components/MovieSuggestionComponent/MovieSuggestionComponent";
+import logo from "./assets/images/logo.png";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
-    </>
-  )
+    <Router>
+      <div className="container-master">
+        <div className="title">
+          <h1>Movie Suggestion App</h1>
+          <img src={logo} />
+        </div>
+
+        <nav className="nav-menu">
+          <button>
+            <Link style={{ textDecoration: "none" }} to="/">
+              HOME
+            </Link>
+          </button>
+          <button>
+            <Link style={{ textDecoration: "none" }} to="/admin/add">
+              ADD MOVIE
+            </Link>{" "}
+          </button>
+          <button>
+            <Link style={{ textDecoration: "none" }} to="/suggest">
+              GET SUGGESTION
+            </Link>{" "}
+          </button>
+        </nav>
+        <Routes>
+          <Route exact path="/" element={<GetAllMoviesComponent />}></Route>
+          <Route path="/admin/add" element={<AddNewMovieComponent />}></Route>
+          <Route path="/suggest" element={<MovieSuggestionComponent />}></Route>
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
